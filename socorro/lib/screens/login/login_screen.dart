c83c59bc8c4012aadb2c9bc307d89e6e78bc4329
ELIAS <eliazzlima@gmail.com>
2024-08-03 12:56:47 -0300

    Tela Login atualizada

--- socorro/lib/screens/login/login_screen.dart
+++ socorro/lib/screens/login/login_screen.dart
@@ -2,68 +2,130 @@ import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
-  Widget build(Object context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Card(
-          margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(16),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-                autocorrect: false,
-                validator: (email) {
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Senha'),
-                autocorrect: false,
-                obscureText: true,
-                validator: (pass) {
-                  if (pass == null || pass.isEmpty || pass.length < 6) {
-                    return 'Senha inválida';
-                  }
-                  return null;
-                },
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blue),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    'Esqueci minha senha',
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 30.0),
+                child: Text(
+                  'Fazer login',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 16,
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 40.0, vertical: 20.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'Digite seu usuário',
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(Icons.person),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 15.0)),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  validator: (email) {
+                    return null;
+                  },
+                ),
               ),
-              SizedBox(
-                height: 44,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors
-                        .white, // Cor de fundo do botão // Padding personalizado
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'Digite sua senha',
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(Icons.key),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 10.0)),
+                  autocorrect: false,
+                  obscureText: true,
+                  validator: (pass) {
+                    if (pass == null || pass.isEmpty || pass.length < 6) {
+                      return 'Senha inválida';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  // Implemente a lógica para "Esqueci minha senha"
+                },
+                child: const Text(
+                  'Esqueci minha senha',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
                   ),
-                  onPressed: () {},
-                  child: const Text(
-                    'Entrar',
-                    style: TextStyle(
-                      fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 70.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 55,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors
+                          .black, // Cor de fundo do botão // Padding personalizado
                     ),
+                    onPressed: () {
+                      // Implemente a lógica para o botão "Entrar"
+                    },
+                    child: Text('Entrar'),
                   ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 70.0, vertical: 30.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 55,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors
+                          .black, // Cor de fundo do botão // Padding personalizado
+                    ),
+                    onPressed: () {
+                      // Implemente a lógica para o botão "Entrar"
+                    },
+                    child: Text('Cadastrar-se'),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30.0),
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  width: 150,
                 ),
               ),
             ],
