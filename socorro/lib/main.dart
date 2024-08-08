bc544316cbf18785280a9d850be67ebe68e388a9
giclayne <giclayne.silva@elife.com.br>
2024-08-08 16:04:39 -0300

    deploy e variaveis de ambiente

--- socorro/lib/main.dart
+++ socorro/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:socorro/screens/base/base_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: String.fromEnvironment(('FIREBASE_API_KEY')),
+          projectId: String.fromEnvironment(('FIREBASE_PROJECT_ID')),
+          storageBucket: String.fromEnvironment(('FIREBASE_STORAGE_BUCKET')),
+          messagingSenderId: String.fromEnvironment(('FIREBASE_MESSAGING_SENDER_ID')),
+          appId: String.fromEnvironment(('FIREBASE_APP_ID'))
+      )
+  );
 
-  //FirebaseFirestore.instance.collection('Teste').add({'Teste': 'teste'});
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
