c8b7a1ec67a82592ef02345591fdd8d430582ec4
Hester910 <hadassa.ester@ufrpe.br>
2024-09-19 22:34:47 -0300

    Renomeação de especialidades

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,29 +1,22 @@
 import React, { useState, useEffect, Drawer } from 'react';
-import { View, Text, FlatList, TouchableOpacity, Image, StyleSheet, ScrollView  } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image, StyleSheet, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons'
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
-
-
-
-
 const data = [
   // Dados fictícios para os cards
-  { id: 1, name: 'João Silva', role: 'Desenvolvedor', image: require('../../assets/images/icon_perfil.png') },
-  { id: 2, name: 'Maria Oliveira', role: 'Designer', image: require('../../assets/images/icon_perfil.png')},
-  { id: 3, name: 'Sandro Ferreira', role: 'Programador', image: require('../../assets/images/icon_perfil.png') },
-  { id: 4, name: 'Pedro Lima', role: 'Engenheiro', image: require('../../assets/images/icon_perfil.png') },
-  { id: 5, name: 'Ana Souza', role: 'Cientista', image: require('../../assets/images/icon_perfil.png') },
-  // ... outros dados
+  { id: 1, name: 'João Silva', role: 'Pedreiro', image: require('../../assets/images/icon_perfil.png') },
+  { id: 2, name: 'Maria Oliveira', role: 'Vidraceiro', image: require('../../assets/images/icon_perfil.png') },
+  { id: 3, name: 'Sandro Ferreira', role: 'Eletricista', image: require('../../assets/images/icon_perfil.png') },
+  { id: 4, name: 'Pedro Lima', role: 'Encanador', image: require('../../assets/images/icon_perfil.png') },
+  { id: 5, name: 'Ana Souza', role: 'Pintor', image: require('../../assets/images/icon_perfil.png') },
 ];
 
 const publicidade = [
-  // Dados fictícios para os cards
   { id: 1, texto: 'anuncie aqui 1' },
   { id: 2, texto: 'anuncie aqui 2' },
   { id: 3, texto: 'anuncie aqui 3' },
-  // ... outros dados
 ];
 
 
@@ -53,75 +46,75 @@ const Home = () => {
   }, []);
 
   return (
-    
+
     <View style={styles.container}>
       <View style={styles.header}>
         <Ionicons name="menu" size={25} color='#0F1626' style={styles.menuIcon} />
         <AntDesign name='search1' size={25} color='#0F1626' style={styles.searchIcon} />
       </View>
       <ScrollView Style={styles.container}>
-      <Text style={styles.title}>Últimos Contratados</Text>
+        <Text style={styles.title}>Últimos Contratados</Text>
 
-      <FlatList
-        data={data}
-        renderItem={({ item }) => <Item name={item.name} role={item.role}  image={item.image} />}
-        keyExtractor={(item) => item.id}
-        horizontal={true} 
-        showsHorizontalScrollIndicator={false} 
-      />
-      <FlatList
-        data={randomAnuncio}
-        renderItem={({ item }) => <Anuncio texto={item.texto} />}
-        keyExtractor={(item) => item.id}
-      />
-      <Text style={styles.title}>Principais Especialidades</Text>
-      <Text style={styles.subTitle}>Desenvolvedor</Text>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => <Item name={item.name} role={item.role}  image={item.image} />}
-        keyExtractor={(item) => item.id}
-        horizontal={true} 
-        showsHorizontalScrollIndicator={false} 
-      />
-      <Text style={styles.subTitle}>Designer</Text>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => <Item name={item.name} role={item.role}  image={item.image} />}
-        keyExtractor={(item) => item.id}
-        horizontal={true} 
-        showsHorizontalScrollIndicator={false} 
-      />
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Item name={item.name} role={item.role} image={item.image} />}
+          keyExtractor={(item) => item.id}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        />
+        <FlatList
+          data={randomAnuncio}
+          renderItem={({ item }) => <Anuncio texto={item.texto} />}
+          keyExtractor={(item) => item.id}
+        />
+        <Text style={styles.title}>Principais Especialidades</Text>
+        <Text style={styles.subTitle}>Pedreiro</Text>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Item name={item.name} role={item.role} image={item.image} />}
+          keyExtractor={(item) => item.id}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        />
+        <Text style={styles.subTitle}>Vidraceiro</Text>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Item name={item.name} role={item.role} image={item.image} />}
+          keyExtractor={(item) => item.id}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        />
 
-      <FlatList
-        data={randomAnuncio}
-        renderItem={({ item }) => <Anuncio texto={item.texto} />}
-        keyExtractor={(item) => item.id}
-      />
+        <FlatList
+          data={randomAnuncio}
+          renderItem={({ item }) => <Anuncio texto={item.texto} />}
+          keyExtractor={(item) => item.id}
+        />
 
-      <Text style={styles.subTitle}>Engenheiro de Software</Text>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => <Item name={item.name} role={item.role}  image={item.image} />}
-        keyExtractor={(item) => item.id}
-        horizontal={true} 
-        showsHorizontalScrollIndicator={false} 
-      />
+        <Text style={styles.subTitle}>Eletricista</Text>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Item name={item.name} role={item.role} image={item.image} />}
+          keyExtractor={(item) => item.id}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        />
 
-      <Text style={styles.subTitle}>Cientista de Dados</Text>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => <Item name={item.name} role={item.role}  image={item.image} />}
-        keyExtractor={(item) => item.id}
-        horizontal={true} 
-        showsHorizontalScrollIndicator={false} 
-      />
+        <Text style={styles.subTitle}>Pintor</Text>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Item name={item.name} role={item.role} image={item.image} />}
+          keyExtractor={(item) => item.id}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        />
 
-<FlatList
-        data={randomAnuncio}
-        renderItem={({ item }) => <Anuncio texto={item.texto} />}
-        keyExtractor={(item) => item.id}
-      />
-    </ScrollView>
+        <FlatList
+          data={randomAnuncio}
+          renderItem={({ item }) => <Anuncio texto={item.texto} />}
+          keyExtractor={(item) => item.id}
+        />
+      </ScrollView>
     </View>
   );
 };
@@ -140,14 +133,14 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'left',
-    marginTop: 10, 
+    marginTop: 10,
     marginBottom: 10,
-
+    
   },
   subTitle: {
     fontSize: 16,
     textAlign: 'left',
-    marginTop: 10, 
+    marginTop: 10,
     marginBottom: 10,
 
   },
