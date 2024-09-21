1a3d7545325e36df2d18ca7a059f0f3d36d90b28
Hester910 <hadassa.ester@ufrpe.br>
2024-09-21 18:39:22 -0300

    Organização de telas da perspectiva do admin

--- app/admin.js
+++ app/admin.js
@@ -1,14 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-
+import { useRouter } from 'expo-router';
 
 export default function admin() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>BEM-VINDO ADMIN</Text>
-    </View>
-  )
+    const router = useRouter();
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.texto}>BEM-VINDO, ADMINISTRADOR</Text>
+            <TouchableOpacity onPress={() => router.push("clientes")} style={styles.button}>
+                <Text style={styles.buttonText}>Clientes</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => router.push("profissionais")} style={styles.button}>
+                <Text style={styles.buttonText}>Profissionais</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => router.push("anunciantes")} style={styles.button}>
+                <Text style={styles.buttonText}>Anunciantes</Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
 
 const styles = {
@@ -16,12 +27,24 @@ const styles = {
         flex: 1,
         alignItems: 'center',
         backgroundColor: '#0F1626',
-        paddingTop: hp('20%'),
+        paddingTop: hp('15%'),
     },
     texto: {
         color: '#FFFFFF',
-        fontSize: hp(4),
+        fontSize: hp(3),
         fontWeight: '600',
         marginBottom: hp('6%'),
-    }
+    },
+    button: {
+        width: '80%',
+        alignItems: 'center',
+        backgroundColor: '#EFC51B',
+        paddingVertical: hp('1.5%'),
+        borderRadius: 10,
+        marginBottom: hp('3%'), // Espaçamento entre os botões
+    },
+    buttonText: {
+        fontSize: hp(2.5),
+        color: '#000000',
+    },
 }
