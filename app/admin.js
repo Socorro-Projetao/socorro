a56715fab80a87b1f2613682c959d9cb29006e38
Hester910 <hadassa.ester@ufrpe.br>
2024-09-21 18:25:44 -0300

    Verificação de perfil admin

---
+++ app/admin.js
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+
+export default function admin() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>BEM-VINDO ADMIN</Text>
+    </View>
+  )
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#0F1626',
+        paddingTop: hp('20%'),
+    },
+    texto: {
+        color: '#FFFFFF',
+        fontSize: hp(4),
+        fontWeight: '600',
+        marginBottom: hp('6%'),
+    }
+}
