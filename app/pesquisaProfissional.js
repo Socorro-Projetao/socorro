8789f5324d87e5419885fcf59c4b59ec86778b9f
giclayne <giclayne.silva@elife.com.br>
2024-10-03 22:52:04 -0300

    ajuste da pesquisa por profissional

--- app/pesquisaProfissional.js
+++ app/pesquisaProfissional.js
@@ -1,35 +1,54 @@
-import { View, TextInput, SafeAreaView, StatusBar, FlatList, TouchableOpacity, Text } from 'react-native'
-import React, { useState } from 'react'
+import { View, TextInput, SafeAreaView, StatusBar, FlatList, TouchableOpacity, Text, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { useRouter } from 'expo-router';
 import { AntDesign } from '@expo/vector-icons'
 import {collection, query, where, getDocs} from 'firebase/firestore'
 import {db} from '../firebaseConfig'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function pesquisaProfissional() {
   const router = useRouter();
 
-  const [search, setSearch] = useState('')
-  const [suggestions, setSuggestions] = useState([])
+  const [search, setSearch] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [filteredProfessionals, setFilteredProfessionals] = useState([]);
   const [isFocused, setIsFocused] = useState(false);
 
-  const filteredSuggestions = suggestions.filter(item =>
-    item.username.toLowerCase().includes(search.toLowerCase())
-  );
+  useEffect(() => {
+    const loadSearch = async () => {
+      try {
+        const savedSearch = await AsyncStorage.getItem('pesquisaProfissional');
+        if (savedSearch) {
+          setSearch(savedSearch);
+          fetchProfessionals(savedSearch);
+        }
+      } catch (error) {
+        console.error('Erro ao carregar pesquisa salva:', error);
+      }
+    };
 
-  // função para buscar profissionais
+    loadSearch();
+  }, []);
+
   const fetchProfessionals = async (searchTerm) => {
     if (searchTerm.length === 0){
       setSuggestions([])
+      setFilteredProfessionals([]);
       return
     }
 
-    const q = query(collection(db, 'professionals'), where('username', '>=', searchTerm), where('username', '<=', searchTerm + '\uf8ff'));
+    const q = query(
+      collection(db, 'professionals'), 
+      where('username', '>=', searchTerm), 
+      where('username', '<=', searchTerm + '\uf8ff')
+    );
 
     const querySnapshot = await getDocs(q);
     const professionalsList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 
     setSuggestions(professionalsList);
+    setFilteredProfessionals(professionalsList);
   }
 
   const handleSuggestionPress = (professional) => {
@@ -39,14 +58,26 @@ export default function pesquisaProfissional() {
     });
   }
 
+  const handleClearSearch = async () => {
+    setSearch('');
+    setSuggestions([]);
+    setFilteredProfessionals([]);
+    await AsyncStorage.removeItem('pesquisaProfissional'); 
+  };
+
+  const handleBackPress = async () => {
+    await handleClearSearch(); 
+    router.push("opcoesPesquisa");
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.searchBar}>
-      <AntDesign 
+        <AntDesign 
           name='arrowleft' 
           size={25} 
           color='#0F1626' 
-          onPress={() => router.push("opcoesPesquisa")} 
+          onPress={handleBackPress} 
           style={styles.iconLeft} 
         />
         <TextInput
@@ -60,6 +91,7 @@ export default function pesquisaProfissional() {
           onChangeText={(text) => {
             setSearch(text);
             fetchProfessionals(text)
+            AsyncStorage.setItem('pesquisaProfissional', text);
           }}
         />
         {search.length === 0 && !isFocused && ( 
@@ -74,23 +106,22 @@ export default function pesquisaProfissional() {
             name='close' 
             size={25} 
             color='#0F1626' 
-            onPress={() => {
-              setSearch('');
-              setSuggestions([]);
-            }} 
+            onPress={handleClearSearch}
             style={styles.iconClose} 
           />
         )}
       </View>
-      {search.length > 0 && (
+      {filteredProfessionals.length > 0 && (
         <FlatList
-          data={filteredSuggestions}
+          data={filteredProfessionals}
           renderItem={({ item }) => (
-            <TouchableOpacity style={styles.suggestionItem} onPress={() => handleSuggestionPress(item)}>
-              <Text style={styles.suggestionText}>{item.username}</Text>
+            <TouchableOpacity style={styles.professionalCard} onPress={() => handleSuggestionPress(item)}>
+              <Image source={{ uri: item.profilePicture }} style={styles.professionalImage} />
+              <Text style={styles.professionalName}>{item.username}</Text>
             </TouchableOpacity>
           )}
           keyExtractor={(item) => item.id}
+          contentContainerStyle={styles.flatListContainer} 
         />
       )}
     </SafeAreaView>
@@ -133,4 +164,45 @@ const styles = {
     fontSize: 16,
     color: '#000',
   },
-}
+  row: {
+    justifyContent: 'space-between',
+    paddingHorizontal: wp(4),
+    marginBottom: wp(3),
+  },
+  professionalImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginBottom: 10,
+  },
+  professionalCard: {
+    backgroundColor: '#f0f0f0',
+    margin: wp(2), 
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    width: wp(28),
+    flex: 1, 
+  },
+  
+  professionalName: {
+    marginTop: wp('2.2%'),
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    flexWrap: 'wrap', 
+    minWidth: wp(25), 
+    maxWidth: wp(28), 
+    overflow: 'hidden', 
+  },
+  vazio: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  flatListContainer: {
+    flexDirection: 'row',
+    justifyContent: 'left', 
+    padding: wp(5), 
+  },
+};
