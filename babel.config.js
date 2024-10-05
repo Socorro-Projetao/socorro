0dd0754e0e5f955b094f2dfd32b1bbdd55a0c0bb
giclayne <giclayne.silva@elife.com.br>
2024-10-04 21:41:46 -0300

    escondendo credenciais importantes

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,7 @@ module.exports = function (api) {
       // Required for expo-router
       //"expo-router/babel",
       "react-native-reanimated/plugin",
+      "module:react-native-dotenv"  
     ]
   };
 };
